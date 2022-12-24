c65978e6d7ccd998f04fda15d9afcb5e76abb2e3
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-24 11:19:58 +0700

    ComponentDidMount React

--- keys.js
+++ keys.js
@@ -1,32 +1,32 @@
-this.state = {
-    monsters [
-        monster1 {
-            name: Hans
-            id: sgfdsf
-        },
-        monster2 {
-            name: Hannah 
-            id: sfsfsd
-        },
+// this.state = {
+//     monsters [
+//         monster1 {
+//             name: Hans
+//             id: sgfdsf
+//         },
+//         monster2 {
+//             name: Hannah 
+//             id: sfsfsd
+//         },
 
-        monster3 {
-            name: Leeham
-            id: sgsssf
-        },
-    ]
-}
+//         monster3 {
+//             name: Leeham
+//             id: sgsssf
+//         },
+//     ]
+// }
 
-render() { // key should be added only after map function is called. Duh! 
-    
-    return(
-        <div className = 'Monsters'>
-        {this.state.monsters.map(monster) => {
-            return (
-                <div key = {monster.id}>
-                <h1>{monster.name}</h1>;)
-                </div>
-        }}
+// render() { // key should be added only after map function is called. Duh! 
+
+//     return(
+//         <div className = 'Monsters'>
+//         {this.state.monsters.map(monster) => {
+//             return (
+//                 <div key = {monster.id}>
+//                 <h1>{monster.name}</h1>;)
+//                 </div>
+//         }}
         
-        </div>
-    )
-}
+//         </div>
+//     )
+// }

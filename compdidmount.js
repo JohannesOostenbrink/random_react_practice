6752c6683bbb156f02523d2a9d85e75262b476ca
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-24 11:28:59 +0700

    componentDidMount

---
+++ compdidmount.js
@@ -0,0 +1,13 @@
+// This is a lifecycle method in order to make an API request for data
+//This done right after constructor in class components
+
+
+// componentDidMount() {
+//     .fetch(url)
+//     .then((response) =>
+//         response.json())
+//     .then(users) => this.setState (() => {
+//         return{monsters:users}
+//     })
+
+// }

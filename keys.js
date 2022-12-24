5f201ecedbf14b08fcb1f89ae035833b25fb4fe7
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-24 10:58:23 +0700

    Keys for mapping

---
+++ keys.js
@@ -0,0 +1,32 @@
+this.state = {
+    monsters [
+        monster1 {
+            name: Hans
+            id: sgfdsf
+        },
+        monster2 {
+            name: Hannah 
+            id: sfsfsd
+        },
+
+        monster3 {
+            name: Leeham
+            id: sgsssf
+        },
+    ]
+}
+
+render() { // key should be added only after map function is called. Duh! 
+    
+    return(
+        <div className = 'Monsters'>
+        {this.state.monsters.map(monster) => {
+            return (
+                <div key = {monster.id}>
+                <h1>{monster.name}</h1>;)
+                </div>
+        }}
+        
+        </div>
+    )
+}

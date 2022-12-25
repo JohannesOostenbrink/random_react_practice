f59920e501195876f158272f4fe087086bed4937
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-25 13:16:40 +0700

    Learning about the searchbox component

--- SearchBox_comp.js
+++ SearchBox_comp.js
@@ -39,3 +39,28 @@
 //doesn't restore to its initial state. Thats becuase the filtered monsters 
 //is in the return statement creating a new state. Instead this should be a function we call on when we need it
 
+// render() {
+//     const filteredMonsters = this.State.monster.filter((monster) => {
+//          return monster.name.toLocaleLowerCase().includes(this.state.searchField)})
+//          // now the problem is that we dont have access to searchString as search string is part of the event onChange
+//          // We can resolve this by adding searchString as part of our state in constructor with an empty ''
+//     return(
+
+        //now we can adjust our functionality as follows 
+
+//         onChange = {(event) => {
+//             const searchString = event.target.value.toLocaleLowerCase
+//             this.setState(() => {
+//                 return {searchString}
+//             })
+//         }}
+
+//     )
+// }
+
+// so essentially what we've done here is we added a searchField/ string to the constructor state so that it lives within our state
+// then we created a function filteredMonsters which is called when the searchField is utilized 
+// it lives in our render but not in our UI return because otherwise filtered monsters will be our new state 
+// then we added functionality to our input search with an event
+// this onChange event compares the value in the searchfield and sets it as state
+// Filtered monsters will display what is written in the searchfield and searchfield value will be set to state 
